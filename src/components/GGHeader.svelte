b35961ace19c507a7868ee292f23bff91cef9dfc
GGe <xngt974@gmail.com>
2023-11-16 23:22:10 +0000

    testing jobs

--- src/components/GGHeader.svelte
+++ src/components/GGHeader.svelte
@@ -1,7 +1,7 @@
 <div class="bg-slate-800 p-2 flex justify-between">
 	<a href="http://ggenerator.ggthe.app" class="flex items-center">
 		<img src="favicon.png" alt="GG Logo" class="w-7" />
-		<h1 class="text-xl">enerator v0.1.0</h1>
+		<h1 class="text-xl">enerator v0.1.1</h1>
 	</a>
 	<!-- Place this tag where you want the button to render. -->
 	<a
