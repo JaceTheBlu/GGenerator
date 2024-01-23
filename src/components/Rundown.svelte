f93703fc071b5e1da2a19a3121c1a47e07ef6dbf
GGe <xngt974@gmail.com>
2024-01-23 11:13:02 +0000

    fix rundown + stiling

--- src/components/Rundown.svelte
+++ src/components/Rundown.svelte
@@ -1,18 +1,60 @@
 <div class="relative w-full h-full">
-	<ul class="flex flex-wrap content-center justify-center w-full h-full text-xl">
-		<li class="rounded-xl bg-slate-800/50 w-8 h-16" />
-		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-16 text-center align-middle ml-2">
-			Test
+	<ul
+		class="flex flex-wrap content-center justify-center w-full h-full text-xl space-x-2 space-y-2 place-items-center"
+	>
+		<div />
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Animal
 		</li>
-		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-16 text-center align-middle ml-2">
-			Test
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			is doing something very funny that could be appointed as
 		</li>
-		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-16 text-center align-middle ml-2">
-			Test
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
 		</li>
-		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-16 text-center align-middle ml-2">
-			Test
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
 		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Action
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			and this is wonderful because then the thing is they went to throw a dice resulting in
+		</li>
+		<li class="rounded-xl bg-slate-800/50 pl-2 pr-2 w-fit h-fit text-center align-middle">
+			@Dice6
+		</li>
+		<li class="cursor rounded-xl bg-slate-800/50 w-8 h-16" />
 	</ul>
 	<button class="rounded-xl bg-slate-800/50 p-4 absolute bottom-0 right-0 font-bold text-2xl"
 		>GGenerate!</button
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <div class="flex flex-grow">
-	<div class="bg-slate-800/50 p-2 rounded-xl br-5 m-4 grow">
+	<div class="bg-slate-800/50 p-2 rounded-xl br-5 m-4 w-3/4">
 		<Rundown />
 	</div>
-	<div class="bg-slate-800/50 p-2 rounded-xl mt-4 mb-4 mr-4 w-1/4 overflow-auto">
+	<div class="bg-slate-800/50 p-2 rounded-xl mt-4 mb-4 mr-4 w-1/4 max-w- overflow-auto">
 		<ListList />
 	</div>
 </div>
