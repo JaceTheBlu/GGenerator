bcfdc20eced8a33c3180c4aae903c0ea1d5187de
gge-think <xngt974@gmail.com>
2024-06-02 12:17:08 +0000

    new footer

--- src/components/GGFooter.svelte
+++ src/components/GGFooter.svelte
@@ -1,1 +1,23 @@
-<div class="bg-slate-800 p-2">GGFooter</div>
+<div class="bg-slate-800 p-2 flex justify-between">
+	GGFooter
+	<div class="flex items-right">
+		<a
+			class="flex items-center"
+			href="https://github.com/JaceTheBlu"
+			aria-label="Follow @JaceTheBlu on GitHub"
+			target="_blank"
+		>
+			<img src="github.png" alt="GitHub Logo" class="w-7 mr-2 pl-2" />
+			@JaceTheBlu🧙‍♂️
+		</a>
+		<a
+			class="flex items-center border-l border-indigo-50 ml-1"
+			href="https://github.com/aVadeleau"
+			aria-label="Follow @aVadeleau on GitHub"
+			target="_blank"
+		>
+			<img src="github.png" alt="GitHub Logo" class="w-7 mr-2 pl-2" />
+			@aVadeleau🎢
+		</a>
+	</div>
+</div>
