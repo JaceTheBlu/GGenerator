86182afbd99a0de8a05b5f29f66a10ac352d6e18
aVadeleau <alanvadeleau@live.fr>
2024-06-03 09:48:06 +0000

    setup colors primaires/secondaires

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import colors from 'tailwindcss/colors';
+
+module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        secondary: colors.yellow[800],
+        neutral: colors.gray,
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
 
