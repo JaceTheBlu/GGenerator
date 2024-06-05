c775a10fc62aa8e971d764d3fc3aa2b6e14c5f14
gge-think <xngt974@gmail.com>
2024-06-05 11:49:12 +0000

    linted + tweaks

--- src/components/Pouch.svelte
+++ src/components/Pouch.svelte
@@ -1,106 +1,116 @@
 <script>
+	/**
+	 * This page represents a pouch, it is possible to add/delete an element from this pouch
+	 * @param name : the name that represents the pouch and the elements in it
+	 * @param pouch_elements : a list that contains every elements a pouch contains, an elements have an id and a name, used to keep on track the existing elements and the display
+	 *
+	 */
 
-    /**
-     * This page represents a pouch, it is possible to add/delete an element from this pouch
-     * @param name : the name that represents the pouch and the elements in it
-     * @param pouch_elements : a list that contains every elements a pouch contains, an elements have an id and a name, used to keep on track the existing elements and the display
-     * 
-    */
+	/* Imports*/
+	import { createEventDispatcher } from 'svelte';
+	import PouchElement from './PouchElement.svelte';
 
-    /* Imports*/
-    import { createEventDispatcher } from 'svelte';
-    import PouchElement from "./PouchElement.svelte";
-    
-    /* Variables*/
-    export let name = "";
+	/* Variables*/
+	export let name = '';
 
-    let pouch_elements = [];
-    let element_id = 0;
-    let new_pouch="";
-    $: pouch_elements_id = name.toLowerCase() + element_id;
-    $: pouch_id = "addPouch" + name;
+	let pouch_elements = [];
+	let element_id = 0;
+	let new_pouch = '';
+	$: pouch_elements_id = name.toLowerCase() + element_id;
+	$: pouch_id = 'addPouch' + name;
 
-    const dispach = createEventDispatcher();
+	const dispach = createEventDispatcher();
 
+	/* Functions */
 
-    /* Functions */
+	/**
+	 * This function handle the add of a elements in the pouch
+	 * @param event : the element to add, with and id and a name property
+	 */
+	function handleKeyDown(event) {
+		if (event.key === 'Enter') {
+			if (new_pouch.trim() !== '') {
+				element_id++;
+				pouch_elements = [
+					...pouch_elements,
+					{
+						id: pouch_elements_id,
+						name: new_pouch
+					}
+				];
+				new_pouch = '';
+			}
+		}
+	}
 
-    /**
-     * This function handle the add of a elements in the pouch
-     * @param event : the element to add, with and id and a name property
-     */
-    function handleKeyDown(event) {
-        if ( event.key === 'Enter') {
-            if(new_pouch.trim()!== ""){
-                element_id++;
-                pouch_elements = [ ...pouch_elements, {
-                        id: pouch_elements_id, 
-                        name: new_pouch
-                },]
-                new_pouch ="";
-            }
-        }
-    }
+	/**
+	 * This function delete an element from a pouch, it catches the event created from "PouchElement.svelte"
+	 * @param e : the id of the pouch to delete
+	 */
+	function deletePouchElement(e) {
+		const deletionId = e.detail;
+		let found = false;
+		let i = 0;
 
-    /**
-     * This function delete an element from a pouch, it catches the event created from "PouchElement.svelte"
-     * @param e : the id of the pouch to delete
-     */
-    function deletePouchElement(e){
-        const deletionId = e.detail;
-        let found = false;
-        let i = 0;
+		while (i < pouch_elements.length && !found) {
+			if (deletionId === pouch_elements[i].id) {
+				pouch_elements.splice(i, 1);
+				found = true;
+			}
+			i++;
+		}
+		pouch_elements = [...pouch_elements];
+	}
 
-        while( (i<pouch_elements.length) && (!found) ){
-            if(deletionId === pouch_elements[i].id){
-                pouch_elements.splice(i,1);
-                found = true;
-            }
-            i++;
-        }
-        pouch_elements = [...pouch_elements];
-
-    }
-
-    /**
-     * This function create an event with the goal of deleting a pouch, it takes the name of the pouch to delete
-     * @param pouchName : the name of the pouch to delete
-     */
-    function dispachDeletePouch(pouchName){
-        dispach('delete-pouch',pouchName);
-    }
+	/**
+	 * This function create an event with the goal of deleting a pouch, it takes the name of the pouch to delete
+	 * @param pouchName : the name of the pouch to delete
+	 */
+	function dispachDeletePouch(pouchName) {
+		dispach('delete-pouch', pouchName);
+	}
 </script>
 
-
 <ul class="bg-slate-800/50 rounded-xl divide-y mt-4 mb-4">
-    <li class="flex justify-between font-bold text-xl h-full pl-2"> 
-        <span class="flex items-center"> > </span>
-        <div class="flex hover:cursor-move">
-            <span class="flex justify-center items-center text-white/50 pr-1" >
-                @
-            </span>
-            <span class="flex justify-center hover:text-orange-500 transition-colors duration-300 items-center">
-                {name}
-            </span>
-        </div>
-        <button class="flex justify-end items-center text-white h-full rounded-tr-xl hover:bg-red-500 transition-colors duration-300 px-2" on:click={dispachDeletePouch(name)}>X</button>
-    </li>
+	<li class="flex justify-between text-xl h-full">
+		<span
+			class="flex items-center px-2 hover:rotate-90 transition duration-300 hover:cursor-pointer rounded-tl-xl"
+		>
+			>
+		</span>
+		<div
+			class="flex hover:cursor-move grow hover:text-orange-500 transition-colors duration-300 justify-center"
+		>
+			<span class="flex items-center text-white/50"> @ </span>
+			<span class="flex font-bold items-center">
+				{name}
+			</span>
+		</div>
+		<button
+			class="flex justify-end items-center text-white h-full rounded-tr-xl hover:bg-red-500 transition-colors duration-300 px-2"
+			on:click={dispachDeletePouch(name)}>X</button
+		>
+	</li>
 
-    {#if pouch_elements.length !== 0}
-        <div class="divide-y divide-dashed">
-            {#each pouch_elements as pouch}
-                <PouchElement id={pouch.id} name={pouch.name} on:delete-pouch-element={deletePouchElement}/>
-            {/each}
-        </div>
-    {/if}
+	{#if pouch_elements.length !== 0}
+		<div class="divide-y divide-dashed">
+			{#each pouch_elements as pouch}
+				<PouchElement
+					id={pouch.id}
+					name={pouch.name}
+					on:delete-pouch-element={deletePouchElement}
+				/>
+			{/each}
+		</div>
+	{/if}
 
-    <li>
-        <input
-            bind:value={new_pouch} 
-            class="bg-transparent w-full h-full focus:outline-none placeholder:italic rounded-b-xl pl-4 pb-1" 
-            type="text"
-            placeholder="Enter a value..."
-            on:keydown={handleKeyDown}
-        >
-    </li>
+	<li>
+		<input
+			bind:value={new_pouch}
+			class="bg-transparent w-full h-full focus:outline-none placeholder:italic rounded-b-xl pl-4 pb-1"
+			type="text"
+			placeholder="Enter a value..."
+			on:keydown={handleKeyDown}
+		/>
+	</li>
 </ul>
--- src/components/PouchElement.svelte
+++ src/components/PouchElement.svelte
@@ -1,32 +1,34 @@
 <script>
-    /**
-     * This component represent a pouch element
-     * A pouch element is represented by : 
-     * @param id : The id of the pouch element
-     * @param name : the name of the pouch element
-    */
+	/**
+	 * This component represent a pouch element
+	 * A pouch element is represented by :
+	 * @param id : The id of the pouch element
+	 * @param name : the name of the pouch element
+	 */
 
-    /* Imports  */
-    import { createEventDispatcher } from 'svelte';
-    
-    /* Variables */
-    export let id = 0;
-    export let name ="";
+	/* Imports  */
+	import { createEventDispatcher } from 'svelte';
 
-    const dispach = createEventDispatcher();
+	/* Variables */
+	export let id = 0;
+	export let name = '';
 
-    /* Functions */
-    /**
-     * This function create an event with the goal of deleting an element from a pouch, it take the id of the element to delete
-     * @param elementId : the id of the element to delete
-     */
-    function dispachDelete(elementId){
-        dispach('delete-pouch-element',elementId);
-    }
+	const dispach = createEventDispatcher();
 
+	/* Functions */
+	/**
+	 * This function create an event with the goal of deleting an element from a pouch, it take the id of the element to delete
+	 * @param elementId : the id of the element to delete
+	 */
+	function dispachDelete(elementId) {
+		dispach('delete-pouch-element', elementId);
+	}
 </script>
 
-<li class="flex justify-between items-center odd:bg-slate-800/10 even:bg-slate-800/50 pl-4 ">
-    <span class=" flex-grow truncate ..." >{name}</span>
-    <button class="text-xl text-white border-l border-white  hover:bg-red-500 transition-colors duration-300 px-2" on:click={dispachDelete(id)}>X</button>
+<li class="flex justify-between items-center odd:bg-slate-800/10 even:bg-slate-800/50 pl-4">
+	<span class=" flex-grow truncate ...">{name}</span>
+	<button
+		class="text-xl text-white border-l border-white hover:bg-red-500 transition-colors duration-300 px-2"
+		on:click={dispachDelete(id)}>X</button
+	>
 </li>
