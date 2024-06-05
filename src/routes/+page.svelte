dfa4b1c181d18dd9fc8aadd7ddb1cf922f115ffc
gge-think <xngt974@gmail.com>
2024-06-05 09:55:24 +0000

    merged layout and page svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script>
+	import '../app.css';
+	import GGFooter from '../components/GGFooter.svelte';
+	import GGHeader from '../components/GGHeader.svelte';
 	import ListList from '../components/ListList.svelte';
 	import Rundown from '../components/Rundown.svelte';
 </script>
 
-<div class="flex flex-grow">
-	<div class="bg-slate-800/50 p-2 rounded-xl br-5 m-4 w-3/4">
-		<Rundown />
+<div class="flex flex-col min-h-screen">
+	<GGHeader />
+	<div class="flex grow">
+		<div class="bg-slate-800/50 p-2 rounded-xl br-5 m-4 w-3/4">
+			<Rundown />
+		</div>
+		<div class="bg-slate-800/50 p-2 rounded-xl mt-4 mb-4 mr-4 w-1/4 max-w- overflow-auto">
+			<ListList />
+		</div>
 	</div>
-	<div class="bg-slate-800/50 p-2 rounded-xl mt-4 mb-4 mr-4 w-1/4 max-w- overflow-auto">
-		<ListList />
-	</div>
+	<GGFooter />
 </div>
