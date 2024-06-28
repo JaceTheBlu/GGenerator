bca952e2af2a32ce7ba69861afe94259e73b9dbf
gge-think <xngt974@gmail.com>
2024-06-28 10:39:31 +0000

    feat(Export) added feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -80,9 +80,23 @@
 		};
 		input.click();
 	};
+
 	const exportJSON = () => {
-		console.log('export');
+		const filename = 'data.json';
+		const jsonStr = JSON.stringify({ rundown: rundown_list, pouch_list: pouch_list });
+
+		let element = document.createElement('a');
+		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonStr));
+		element.setAttribute('download', filename);
+
+		element.style.display = 'none';
+		document.body.appendChild(element);
+
+		element.click();
+
+		document.body.removeChild(element);
 	};
+
 	const createPouchIfNE = (event) => {
 		console.log(event);
 		let pouchName = event.detail.text.substring(1);
